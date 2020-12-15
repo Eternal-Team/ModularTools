2b5183b760f2198dd5dfab880370839bc4f12c8e
Itorius <itorius.yt@gmail.com>
2020-12-15 21:43:07 +0100

    Custom damage class

---
+++ Content/ModularDamageClass.cs
@@ -0,0 +1,12 @@
+﻿using Terraria.ModLoader;
+
+namespace ModularTools.Content
+{
+	public class ModularDamageClass: DamageClass
+	{
+		public override void SetupContent()
+		{
+			ClassName.SetDefault("energy damage");
+		}
+	}
+}
