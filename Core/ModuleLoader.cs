52f756f6043a327d4af3c7195eb05fc423a6fd45
Itorius <itorius.yt@gmail.com>
2021-01-15 22:02:59 +0100

    Incompatible module mechanic

--- Core/ModuleLoader.cs
+++ Core/ModuleLoader.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using Terraria.ModLoader;
+using Terraria.ModLoader.Tags;
 
 namespace ModularTools.Core
 {
@@ -12,7 +14,12 @@ namespace ModularTools.Core
 
 		internal static Dictionary<int, List<int>> requirements = new Dictionary<int, List<int>>();
 
+		internal static Dictionary<int, List<int>> blacklistTypes = new Dictionary<int, List<int>>();
+		internal static Dictionary<int, List<TagData>> blacklistGroups = new Dictionary<int, List<TagData>>();
+
 		internal static int NextTypeID;
+
+		public static int Count => NextTypeID;
 
 		internal static void RegisterModule(BaseModule module)
 		{
@@ -22,9 +29,37 @@ namespace ModularTools.Core
 			validItemsForModule.Add(module.Type, new List<int>());
 
 			requirements.Add(module.Type, new List<int>());
+			blacklistTypes.Add(module.Type, new List<int>());
+			blacklistGroups.Add(module.Type, new List<TagData>());
 		}
 
-		public static int ModuleType<T>() where T : ModItem => ModContent.GetInstance<T>()?.Type ?? 0;
+		public static int ModuleType<T>() where T : BaseModule => ModContent.GetInstance<T>()?.Type ?? -1;
+
+		public static BaseModule GetModule(int type) => modules[type];
+
+		public static IEnumerable<int> GetRequirements<T>() where T : BaseModule => requirements[ModuleType<T>()];
+
+		public static IEnumerable<int> GetRequirements(int type) => requirements[type];
+
+		public static IEnumerable<int> GetIncompatibleModules<T>() where T : BaseModule
+		{
+			int type = ModuleType<T>();
+
+			foreach (int module in blacklistTypes[type]) yield return module;
+			foreach (int module in blacklistGroups[type].SelectMany(tag => tag.GetEntries()))
+			{
+				if (module != type) yield return module;
+			}
+		}
+
+		public static IEnumerable<int> GetIncompatibleModules(int type)
+		{
+			foreach (int module in blacklistTypes[type]) yield return module;
+			foreach (int module in blacklistGroups[type].SelectMany(tag => tag.GetEntries()))
+			{
+				if (module != type) yield return module;
+			}
+		}
 
 		public static T CreateInstance<T>() where T : BaseModule
 		{
