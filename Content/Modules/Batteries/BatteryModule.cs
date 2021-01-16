d4e9d5ca469d11e80a9797d0298a23a794a70cc4
Itorius <itorius.yt@gmail.com>
2021-01-16 20:40:56 +0100

    Base battery module

---
+++ Content/Modules/Batteries/BatteryModule.cs
@@ -0,0 +1,29 @@
+﻿using BaseLibrary.Utility;
+using ModularTools.Core;
+
+namespace ModularTools.Content.Modules
+{
+	public abstract class BatteryModule : BaseModule
+	{
+		protected abstract long EnergyCapacity { get; }
+
+		public override void SetStaticDefaults()
+		{
+			Tooltip.SetDefault($"Increases energy storage by {TextUtility.ToSI(EnergyCapacity)}J");
+
+			ModuleTags.Battery.Set(Type, true);
+
+			AddValidModularItems(ModularItemTags.All);
+		}
+
+		public override void OnInstalled(ModularItem item)
+		{
+			item.EnergyStorage.ModifyCapacity(EnergyCapacity);
+		}
+
+		public override void OnRemoved(ModularItem item)
+		{
+			item.EnergyStorage.ModifyCapacity(-EnergyCapacity);
+		}
+	}
+}
--- Content/Modules/Batteries/NickelIronBattery.cs
+++ Content/Modules/Batteries/NickelIronBattery.cs
@@ -1,39 +1,16 @@
-﻿using BaseLibrary.Utility;
-using ModularTools.Core;
-using Terraria;
-using Terraria.ID;
+﻿using Terraria.ID;
 
 namespace ModularTools.Content.Modules
 {
-	public class NickelIronBattery : BaseModule
+	public class NickelIronBattery : BatteryModule
 	{
-		public override void SetStaticDefaults()
-		{
-			Tooltip.SetDefault($"Increases energy storage by {TextUtility.ToSI(EnergyCapacity)}");
-			EnergyCapacity = 3600 * 25 * 5;
-
-			ModuleTags.Battery.Set(Type, true);
-
-			AddValidModularItems(ModularItemTags.All);
-		}
+		protected override long EnergyCapacity => 3600 * 25 * 5;
 
 		public override void AddRecipes()
 		{
 			Create()
 				.AddIngredient(ItemID.IronBar, 12)
 				.Register();
-		}
-
-		public override void OnInstalled(ModularItem item)
-		{
-		}
-
-		public override void OnRemoved(ModularItem item)
-		{
-		}
-
-		public override void OnUpdate(ModularItem item, Player player)
-		{
 		}
 	}
 }
